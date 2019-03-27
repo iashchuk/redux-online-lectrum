dfa6bc1e1e1623085f554034ae4253be93030468
iashchuk <221box@list.ru>
2019-03-28 00:00:14 +0300

    Подключает redux-logger

--- source/basic-redux/init/store.js
+++ source/basic-redux/init/store.js
@@ -1,6 +1,20 @@
 //Core
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+
 //Reducer
 import { rootReducer } from "./rootReducer";
 
-export const store = createStore(rootReducer);
+const logger = createLogger({
+    duration:  true,
+    collapsed: true,
+    colors:    {
+        title:     () => "#139BFE",
+        prevState: () => " #1C5FAF",
+        action:    () => "#149945",
+        nextState: () => "#A47104",
+        error:     () => "#FF0005",
+    },
+});
+
+export const store = createStore(rootReducer, applyMiddleware(logger));
