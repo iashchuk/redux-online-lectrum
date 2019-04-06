b1463574fa803a4dd70d65524d0929d0bb0bd195
iashchuk <221box@list.ru>
2019-04-07 00:20:04 +0300

    Оптимизирует store. Выделяет middleware

--- source/init/store.js
+++ source/init/store.js
@@ -1,24 +1,10 @@
 //Core
-import { createStore, applyMiddleware, compose } from "redux";
-import { createLogger } from "redux-logger";
+import { createStore } from "redux";
 
 //Reducer
 import { rootReducer } from "./rootReducer";
 
-const logger = createLogger({
-    duration:  true,
-    collapsed: true,
-    colors:    {
-        title:     () => "#139BFE",
-        prevState: () => " #1C5FAF",
-        action:    () => "#149945",
-        nextState: () => "#A47104",
-        error:     () => "#FF0005",
-    },
-});
+// Enhancer
+import { enhancedStore } from "./middleware/core";
 
-const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const composeEnhancers = devtools ? devtools : compose;
-const enhansedStore = composeEnhancers(applyMiddleware(logger));
-
-export const store = createStore(rootReducer, enhansedStore);
+export const store = createStore(rootReducer, enhancedStore);
