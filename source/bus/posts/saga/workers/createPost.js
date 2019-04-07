9bf7a17cf92cdb913f74c393d4ca5a9fe6bd2f18
iashchuk <221box@list.ru>
2019-04-08 00:09:58 +0300

    Реализует отображение индикатора загрузки

--- source/bus/posts/saga/workers/createPost.js
+++ source/bus/posts/saga/workers/createPost.js
@@ -4,9 +4,12 @@ import { put, apply } from "redux-saga/effects";
 // Instruments
 import { api } from "../../../../REST";
 import { postsActions } from "../../actions";
+import { uiActions } from "../../../ui/actions";
 
 export function* createPost ({ payload: comment }) {
     try {
+        yield put(uiActions.startFetching());
+
         const response = yield apply(api, api.posts.create, [comment]);
         const { data: post, message } = yield apply(response, response.json);
 
@@ -16,6 +19,8 @@ export function* createPost ({ payload: comment }) {
 
         yield put(postsActions.createPost(post));
     } catch (error) {
-        console.log("-> createPost worker", error);
+        yield put(uiActions.emitError(error.message, "→ createPost worker"));
+    } finally {
+        yield put(uiActions.stopFetching());
     }
 }
