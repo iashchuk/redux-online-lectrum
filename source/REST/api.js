c3e020dbc6fc7143bb7d3d2e1ea26a6178476b49
iashchuk <221box@list.ru>
2019-04-26 22:58:30 +0300

    Реализует метод api для logout

--- source/REST/api.js
+++ source/REST/api.js
@@ -34,6 +34,14 @@ export const api = {
                 body: JSON.stringify({ token: this.token }),
             });
         },
+        logout () {
+            return fetch(`${MAIN_URL}/user/logout`, {
+                method:  "GET",
+                headers: {
+                    Authorization: this.token,
+                },
+            });
+        },
     },
     posts: {
         fetch () {
